fe87cb3243c46affa2eb93e9e9b86c10af46fe5a
Piotr Kalman <piotr.kalman85@gmail.com>
2021-07-21 22:00:46 +0200

    add animation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const mainContainer = document.querySelector('.main-container');
+
 const bubble_create = () => {
-  
+  let newBubble = document.createElement('div');
+  newBubble.classList.add('bubble');
+  mainContainer.appendChild(newBubble);
+  console.log(newBubble);
 };
 
 window.addEventListener('click', bubble_create);
