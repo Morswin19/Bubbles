ef5e748566de45ca4de1c90a14af5ae71d5ccebc
Piotr Kalman <piotr.kalman85@gmail.com>
2021-07-22 21:47:16 +0200

    add random colors

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bubble_create = () => {
   newBubble.classList.add('bubble');
   mainContainer.appendChild(newBubble);
   console.log(newBubble);
+  let h = Math.round(Math.random() * 360);
+  let s = Math.round(Math.random() * 100);
+  let l = Math.round(Math.random() * 100);
+  console.log(h, s, l);
+  let newColor = `hsl(${h},${s}%,${l}%)`;
+  console.log(newColor);
+  newBubble.style.background = newColor;
 };
 
 window.addEventListener('click', bubble_create);
