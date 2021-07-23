190bfd61965de3dc1f1f44e1d020baf195639f3c
Piotr Kalman <piotr.kalman85@gmail.com>
2021-07-23 20:46:05 +0200

    add some styles, remove logs

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const bubble_create = () => {
   let newBubble = document.createElement('div');
   newBubble.classList.add('bubble');
   mainContainer.appendChild(newBubble);
-  console.log(newBubble);
   let h = Math.round(Math.random() * 360);
-  let s = Math.round(Math.random() * 100);
+  let s = 100;
   let l = Math.round(Math.random() * 100);
-  console.log(h, s, l);
   let newColor = `hsl(${h},${s}%,${l}%)`;
-  console.log(newColor);
   newBubble.style.background = newColor;
 };
 
