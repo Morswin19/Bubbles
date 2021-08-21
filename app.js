3a57002c6743f15df209a7f0b0a0fa9076d75d27
Piotr <piotr.kalman85@gmail.com>
2021-08-21 19:12:54 +0200

    add Bubble class

---
+++ app.js
@@ -0,0 +1,14 @@
+class Bubble {
+    constructor(width, xPosition, speed){
+        this.width = width;
+        this.height = width;
+        this.xPosition = xPosition;
+        this.speed = speed;
+        this.
+    }
+    //events
+    //methods
+    createBubble(){
+
+    }
+}
